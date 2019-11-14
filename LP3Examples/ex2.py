d6592c75a2bb362d79bee549fe2498d7244a674a
Sam Kinsella <SamKinsella@Sams-MBP.dhcp.umsl.edu>
2019-11-13 18:16:00 -0600

    Whatever I want to.

---
+++ LP3Examples/ex2.py
@@ -0,0 +1,9 @@
+#comment number 1
+#comment number 2
+
+print("I could have code like this.")#then a comment right after
+
+#comments disable code
+#print("This won't print.")
+
+print("This will run.")
---
+++ LP3Examples/ex5.py
@@ -0,0 +1,18 @@
+my_name = 'Sam Kinsella'
+my_age = 20
+my_height = 70 #inches
+my_weight = 245 #lbs
+my_eyes = 'blue'
+my_teeth = 'white'
+my_hair = 'light brown'
+
+print(f"Let's talks about {my_name}.")
+print(f"He's {my_height} inches tall.")
+print(f"He's {my_weight} pounds heavy.")
+print(f"Actually that's not too heavy...")
+print(f"He's got {my_eyes} eyes and {my_hair} hair.")
+print(f"His teeth are usually {my_teeth} depending on the coffee.")
+
+#this line is tricky, try to get it exactly right
+total = my_age + my_height + my_weight
+print(f"If I add {my_age}, {my_height}, and {my_weight} I get {total}.")
